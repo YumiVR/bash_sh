6faa828145dcdda20de3e720521ef41877fc480d
Yumi Verma <yumiverma@lilly.student.staffs.ac.uk>
2022-11-07 12:08:13 +0000

    updated

--- checkargs.sh
+++ checkargs.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+echo "Welcome to my script"
+
 if [ $# -eq 0 ]; then
 		echo "You must provide at least 1 argument"
+		exit 1
+else
+		echo "Thank you for your argument $1"
+
 fi
+
+echo "Thanks for running my script"
+
+exit 0
